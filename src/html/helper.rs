fff5063e416fc46bc0f5ddd8bb6e39b7832ce0bf
whichxjy <whichxjy@gmail.com>
2020-05-12 19:46:18 +0800

    Init HTML handler

---
+++ src/html/helper.rs
@@ -0,0 +1,19 @@
+use regex::{Captures, Regex};
+use std::collections::HashMap;
+
+use crate::util;
+
+pub fn replace_macros(text: &String, macros: &HashMap<&'static str, String>) -> String {
+    lazy_static! {
+        static ref REG: Regex = Regex::new(r"\[(?P<inner_text>[A-Z0-9-]+)\]").unwrap();
+    }
+    let replacer = |caps: &Captures| -> String {
+        let inner_text = (&caps["inner_text"]).to_lowercase();
+        if macros.contains_key(inner_text.as_str()) {
+            macros.get(inner_text.as_str()).unwrap().to_string()
+        } else {
+            (&caps[0]).to_string()
+        }
+    };
+    util::regex::replace_all(&REG, text, replacer)
+}
