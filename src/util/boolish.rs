a08059d4257dbba91596680b4153928d50f242d5
whichxjy <whichxjy@gmail.com>
2020-05-27 06:33:46 +0800

    Simplify the boolish-to-bool converter

--- src/util/boolish.rs
+++ src/util/boolish.rs
@@ -1,22 +1,7 @@
-use std::collections::HashSet;
-
 pub fn boolish_to_bool(val: &str) -> Option<bool> {
-    lazy_static! {
-        pub static ref TRUE_VALUES: HashSet<&'static str> = {
-            hashset! {"true", "yes", "y", "on"}
-        };
-        pub static ref FALSE_VALUES: HashSet<&'static str> = {
-            hashset! {"false", "no", "n", "off"}
-        };
-    }
-
-    let val = val.to_lowercase();
-
-    if TRUE_VALUES.contains(&val.as_ref()) {
-        Some(true)
-    } else if FALSE_VALUES.contains(&val.as_ref()) {
-        Some(false)
-    } else {
-        None
+    match &*val.to_lowercase() {
+        "true" | "yes" | "y" | "on" => Some(true),
+        "false" | "no" | "n" | "off" => Some(false),
+        _ => None,
     }
 }
