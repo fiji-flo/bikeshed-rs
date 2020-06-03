b2c98880a535c0f37ce3a40f73d1cd55c579a367
whichxjy <whichxjy@gmail.com>
2020-06-03 21:49:52 +0800

    Ignore headings in boilerplate

--- src/heading.rs
+++ src/heading.rs
@@ -1,4 +1,5 @@
-use kuchiki::{NodeDataRef, NodeRef};
+use kuchiki::{NodeData, NodeDataRef, NodeRef};
+use markup5ever::LocalName;
 
 use crate::html;
 use crate::spec::Spec;
@@ -9,7 +10,23 @@ pub fn process_headings(doc: &mut Spec) {
     // 2. [settled] or not?
     if let Some(ref dom) = doc.dom {
         if let Ok(headings) = dom.select("h2, h3, h4, h5, h6") {
-            let headings = headings.collect::<Vec<NodeDataRef<_>>>();
+            // TODO: Find a better way to filter headings.
+            let headings = headings
+                .filter(|h| {
+                    // [ignore headings in boilerplate]
+                    // [just for testing]
+                    if let NodeData::Element(h_data) = h.as_node().data() {
+                        let ref mut attributes = h_data.attributes.borrow();
+
+                        if let Some(id) = attributes.get(LocalName::from("id")) {
+                            if id == "contents" {
+                                return false;
+                            }
+                        }
+                    }
+                    true
+                })
+                .collect::<Vec<NodeDataRef<_>>>();
 
             for heading in headings {
                 html::node::add_class(heading.as_node(), "heading");
--- src/spec.rs
+++ src/spec.rs
@@ -106,13 +106,13 @@ impl<'a> Spec<'a> {
     }
 
     fn process_document(&mut self) {
-        heading::process_headings(self);
         boilerplate::add_canonical_url(self);
         boilerplate::add_spec_metadata_section(self);
         boilerplate::add_copyright_section(self);
         boilerplate::add_abstract_section(self);
         boilerplate::add_toc_section(self);
         boilerplate::add_bikeshed_boilerplate(self);
+        heading::process_headings(self);
     }
 
     pub fn finish(&self, outfile: Option<&str>) {
