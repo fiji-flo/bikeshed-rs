8e5611954d1f07d9fb8dc9e2515206a010cecf1c
whichxjy <whichxjy@gmail.com>
2020-06-03 08:05:54 +0800

    Reset headings

--- src/heading.rs
+++ src/heading.rs
@@ -1,4 +1,4 @@
-use kuchiki::NodeDataRef;
+use kuchiki::{NodeDataRef, NodeRef};
 
 use crate::html;
 use crate::spec::Spec;
@@ -14,7 +14,24 @@ pub fn process_headings(doc: &mut Spec) {
             for heading in headings {
                 html::node::add_class(heading.as_node(), "heading");
                 html::node::add_class(heading.as_node(), "settled");
+                reset_heading(heading.as_node());
             }
         }
     }
+}
+
+// Insert the content of heading into a <span class='content'> element.
+fn reset_heading(heading_el: &NodeRef) {
+    let content_el = html::node::new_element(
+        "span",
+        btreemap! {
+            "class" => "content".to_owned(),
+        },
+    );
+
+    for child in heading_el.children() {
+        content_el.append(child);
+    }
+
+    heading_el.append(content_el);
 }
