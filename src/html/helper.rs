fdf62ee432862c178f89385dd11674a2d0d5ebcf
whichxjy <whichxjy@gmail.com>
2020-06-22 10:09:21 +0800

    Add a comment about macros-replacer

--- src/html/helper.rs
+++ src/html/helper.rs
@@ -3,10 +3,12 @@ use std::collections::HashMap;
 
 use crate::util;
 
+// Replace macros with text.
 pub fn replace_macros(text: &str, macros: &HashMap<&'static str, String>) -> String {
     lazy_static! {
         static ref REG: Regex = Regex::new(r"\[(?P<inner_text>[A-Z0-9-]+)\]").unwrap();
     }
+
     let replacer = |caps: &Captures| -> String {
         let inner_text = (&caps["inner_text"]).to_lowercase();
         if macros.contains_key(inner_text.as_str()) {
@@ -15,5 +17,6 @@ pub fn replace_macros(text: &str, macros: &HashMap<&'static str, String>) -> Str
             (&caps[0]).to_string()
         }
     };
+
     util::regex::replace_all(&REG, text, replacer)
 }
