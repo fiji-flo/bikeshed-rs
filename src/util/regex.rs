f1a88c6b0010bf5f96929dbad79ee8e4e8280343
whichxjy <whichxjy@gmail.com>
2020-06-24 04:04:47 +0800

    Add tests for string-replacer

--- src/util/regex.rs
+++ src/util/regex.rs
@@ -14,3 +14,50 @@ pub fn replace_all(reg: &Regex, haystack: &str, replacer: impl Fn(&Captures) -> 
     new.push_str(&haystack[last_match..]);
     new
 }
+
+#[cfg(test)]
+mod tests {
+    use super::replace_all;
+    use regex::{Captures, Regex};
+
+    #[test]
+    fn test_replace_all() {
+        {
+            let reg = Regex::new(r"-*green-*").unwrap();
+
+            let replacer = |_: &Captures| -> String { "violet".to_owned() };
+
+            assert_eq!(replace_all(&reg, "red", replacer), "red");
+            assert_eq!(
+                replace_all(&reg, "red green blue", replacer),
+                "red violet blue"
+            );
+            assert_eq!(
+                replace_all(&reg, "red ---green--- blue", replacer),
+                "red violet blue"
+            );
+        }
+        {
+            let reg = Regex::new(r"(-*(?P<color>\w+)-*)").unwrap();
+
+            let replacer = |caps: &Captures| -> String {
+                let color = caps["color"].to_lowercase();
+                if color == "none" {
+                    "violet".to_owned()
+                } else {
+                    color.to_owned()
+                }
+            };
+
+            assert_eq!(replace_all(&reg, "red", replacer), "red");
+            assert_eq!(
+                replace_all(&reg, "red -green- blue", replacer),
+                "red green blue"
+            );
+            assert_eq!(
+                replace_all(&reg, "red ---none--- blue", replacer),
+                "red violet blue"
+            );
+        }
+    }
+}
