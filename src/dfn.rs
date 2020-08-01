f5b5faf01d38760d6387458682f19d5893f27ad2
whichxjy <whichxjy@gmail.com>
2020-08-01 14:39:26 +0800

    Fill in dfn ids

--- src/dfn.rs
+++ src/dfn.rs
@@ -1,5 +1,6 @@
 use kuchiki::NodeRef;
 
+use crate::config;
 use crate::html;
 use crate::spec::Spec;
 
@@ -18,6 +19,13 @@ fn classify_dfns(dfn_els: &[NodeRef]) {
 
         if !html::has_attr(dfn_el, "data-dfn-type") {
             html::insert_attr(dfn_el, "data-dfn-type", dfn_type);
+        }
+
+        // Fill in id if necessary.
+        if !html::has_attr(dfn_el, "id") {
+            let content = html::get_text_content(dfn_el);
+            let id = format!("propdef-{}", config::generate_name(&content));
+            html::insert_attr(dfn_el, "id", id);
         }
     }
 }
