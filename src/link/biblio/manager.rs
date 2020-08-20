99223a7e61d730329ba462b38c351cdc101a4a96
whichxjy <whichxjy@gmail.com>
2020-08-21 02:03:40 +0800

    Support informative biblio entry

--- src/link/biblio/manager.rs
+++ src/link/biblio/manager.rs
@@ -1,4 +1,4 @@
-use super::source::BiblioEntrySource;
+use super::source::{BiblioEntrySource, BiblioFormat};
 use super::BiblioEntry;
 
 #[derive(Debug, Default)]
@@ -13,7 +13,19 @@ impl BiblioEntryManager {
         }
     }
 
-    pub fn get_biblio_entry(&mut self, spec: &str) -> Option<BiblioEntry> {
-        self.biblio_entry_source.fetch_biblio_entry(spec)
+    pub fn get_biblio_entry(&mut self, link_text: &str) -> Option<BiblioEntry> {
+        let link_text = link_text.to_lowercase();
+
+        if let Some(biblio_entry) = self.biblio_entry_source.fetch_biblio_entry(&link_text) {
+            if biblio_entry.biblio_format == BiblioFormat::Alias {}
+            match biblio_entry.biblio_format {
+                BiblioFormat::Alias => {
+                    return self.get_biblio_entry(biblio_entry.alias_of.as_ref().unwrap())
+                }
+                _ => return Some(biblio_entry),
+            }
+        }
+
+        None
     }
 }
